9f26cba750d5140c4e47cdbd979a2556fc404aa6
Mohamed Idris <midris77@ccs.local>
2021-09-10 15:43:57 -0400

    adding my first file

---
+++ blink.py
@@ -0,0 +1,12 @@
+# Write your code here :-)
+import board
+import neopixel
+import time
+
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness= 0.3)
+while True:
+    print("Make it blue!")
+    dot.fill((0,0,255))
+    time.sleep(.5)
+    dot.fill((255, 255, 0))
+    time.sleep(.5)
